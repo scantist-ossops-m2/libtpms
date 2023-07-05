f81f9dc4f27788228f680532ddd3d465e49f463f
Stefan Berger <stefanb@linux.ibm.com>
2023-07-05 07:55:19 -0400

    Revert "Debugging patch"
    This reverts commit 236686310a6f568aa0824df796af74472f774411.

--- src/tpm2/NVMarshal.c
+++ src/tpm2/NVMarshal.c
@@ -5113,6 +5113,7 @@ PERSISTENT_ALL_Unmarshal(BYTE **buffer, INT32 *size)
     memset(&scd, 0, sizeof(scd));
     memset(indexOrderlyRam, 0, sizeof(indexOrderlyRam));
 
+
     if (rc == TPM_RC_SUCCESS) {
         rc = NV_HEADER_Unmarshal(&hdr, buffer, size,
                                  PERSISTENT_ALL_VERSION,
@@ -5131,14 +5132,6 @@ PERSISTENT_ALL_Unmarshal(BYTE **buffer, INT32 *size)
         /* allow all algorithms to be unmarshalled */
         rc = RuntimeAlgorithmSetProfile(&g_RuntimeProfile.RuntimeAlgorithm, NULL, &stateFormatLevel, ~0);
     }
-#if 1
-    if (rc == TPM_RC_SUCCESS) {
-        rc = RuntimeProfileFormatJSON(&g_RuntimeProfile);
-    }
-    if (rc == TPM_RC_SUCCESS) {
-        fprintf(stderr, "Temp profile while unmarshalling: %s\n", RuntimeProfileGetJSON(&g_RuntimeProfile));
-    }
-#endif
     if (rc == TPM_RC_SUCCESS) {
         rc = PACompileConstants_Unmarshal(buffer, size);
     }
@@ -5193,13 +5186,7 @@ skip_future_versions:
         NvWrite(NV_STATE_CLEAR_DATA, sizeof(scd), &scd);
         NvWrite(NV_INDEX_RAM_DATA, sizeof(indexOrderlyRam), indexOrderlyRam);
         /* Activate a profile read from the state of the TPM 2 */
-        fprintf(stderr, "profile from state: %s\n", profileJSON);
         rc = RuntimeProfileSet(&g_RuntimeProfile, profileJSON, false);
-#if 1
-        if (rc == TPM_RC_SUCCESS) {
-            fprintf(stderr, "Final profile after unmarshalling: %s\n", RuntimeProfileGetJSON(&g_RuntimeProfile));
-        }
-#endif
     }
 
     free(profileJSON);
